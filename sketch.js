8ca173777df95b249e5f6b549d53b3f47012a715
pratyush090506 <68912154+pratyush090506@users.noreply.github.com>
2021-07-14 21:58:06 +0530

    Create sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,12 @@
-var fixedrect,movingrect;
-
 function setup() {
   createCanvas(800,400);
 
-  fixedrect = createSprite(400, 200, 30, 60);
-  fixedrect.shapeColor = "green";
-  fixedrect.debug=true;
-  movingrect = createSprite(200,100,60,30);
-  movingrect.shapeColor = "green";
-  movingrect.debug=true;
-}
+if(keyIsDown(UP_ARROW){
+   background(255,0,0);}
 
-function draw() {
-
-  movingrect.x=World.mouseX;
-  movingrect.y=World.mouseY;
-
-  if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2
-    && fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2
-    && movingrect.y-fixedrect.y<fixedrect.width/2+movingrect.width/2
-    && fixedrect.y-movingrect.y<fixedrect.width/2+movingrect.width/2
-    )
-  {
-    movingrect.shapeColor = "red";
-    fixedrect.shapeColor = "red";
-    
-  }
-  else{
-    movingrect.shapeColor = "green";
-    fixedrect.shapeColor = "green";
-  }
-
+else(keyIsDown(DOWN_ARROW){
+   background(0,255,0);}
+   
   background(0,0,0);  
   drawSprites();
 }
